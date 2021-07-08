f8e0aa9f5a4c1260d5e4e04efd2fb037c50f0c58
DmitryBorodiy <winsoft.program@outlook.com>
2021-07-08 21:45:10 +0300

    fix

--- www/main.js
+++ www/main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+window.onload = function(){
+  var width = document.documentElement.clientWidth;
+  
+  if (width <= 850) {
+    document.getElementById("app_navigation").style.visibility = "collapse";
+    document.getElementById("download_app_button").style.visibility = "collapse";
+  }
+  else if (width >= 850) {
+    document.getElementById("app_navigation").style.visibility = "visible";
+    document.getElementById("download_app_button").style.visibility = "visible";
+  }
+}
+
+function Page_OnResize(){
+  var width = document.documentElement.clientWidth;
+  
+  if(width <= 850){
+    document.getElementById("app_navigation").style.visibility = "collapse";
+  }
+  else if(width >= 850){
+    document.getElementById("app_navigation").style.visibility = "visible";
+  }
+}
