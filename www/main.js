1866e4b03a9680834c8d3849690fae13260487ca
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2021-07-11 00:42:37 +0300

    commit

--- www/main.js
+++ www/main.js
@@ -16,14 +16,20 @@ window.onload = function(){
 };
 
 function Page_OnResize() {
-  var width = document.documentElement.scrollWidth;
+    var width = document.documentElement.scrollWidth;
   
-  if(width <= 850){
-    document.getElementById("app_navigation").style.visibility = "collapse";
-  }
-  else if(width >= 850){
-    document.getElementById("app_navigation").style.visibility = "visible";
-  }
+    if (width <= 850) {
+        document.getElementById("app_navigation").style.visibility = "collapse";
+        document.getElementById("download_app_button").style.visibility = "collapse";
+
+        document.getElementById("button_menu").style.visibility = "visible";
+    }
+    else if (width >= 850) {
+        document.getElementById("app_navigation").style.visibility = "visible";
+        document.getElementById("download_app_button").style.visibility = "visible";
+
+        document.getElementById("button_menu").style.visibility = "collapse";
+    }
 }
 
 window.onresize = Page_OnResize;
