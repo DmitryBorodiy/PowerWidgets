1ac6a5d0d538421d9b015afcf0a5db0234cd6ae7
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2021-07-26 21:25:57 +0300

    fix

--- www/download.js
+++ www/download.js
@@ -1,3 +1,18 @@
+window.onload = function(){
+  Window_OnResize();
+};
+
+function Window_OnResize(){
+  var width = document.documentElement.scrollWidth;
+  
+  if (width <= 600){
+    document.getElementById("menu_nav").style.visibility = "collapse";
+  }
+  else{
+    document.getElementById("menu_nav").style.visibility = "visible";
+  }
+}
+
 function SendReview(){
     try
     {
