887aeaa17fe344a6eee40071341b348682617d5f
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2021-07-28 16:29:37 +0300

    Fix

--- www/insider/index.js
+++ www/insider/index.js
@@ -1,3 +1,16 @@
 window.onload = function(){
-    
+    Window_OnResize();
+
+    document.body.onresize = Window_OnResize;
 };
+
+function Window_OnResize(){
+    var width = document.documentElement.scrollWidth;
+    
+    if (width <= 600){
+      document.getElementById("menu_nav").style.visibility = "collapse";
+    }
+    else{
+      document.getElementById("menu_nav").style.visibility = "visible";
+    }
+}
