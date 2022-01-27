2aa22891c53a592a16985274d4df4d77b711ba35
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2022-01-28 00:59:31 +0200

    fix

--- blog/index.js
+++ blog/index.js
@@ -6,11 +6,14 @@ function Content_OnResize(){
   var documentWidthProperty = document.documentElement.clientWidth;
   
   if(documentWidthProperty <= 750){
-    document.getElementById("content_div").style.paddingLeft = "10px";
-    document.getElementById("content_div").style.paddingRight = "10px";
+    //document.getElementById("content_div").style.paddingLeft = "10px";
+    //document.getElementById("content_div").style.paddingRight = "10px";
     
     $('#navbar-logo').css("position", "relative");
-    $('#navbar-logo').css("left", "0px");
+      $('#navbar-logo').css("left", "0px");
+
+      $('#content-div-state').css("padding-left", "15px");
+      $('#content-div-state').css("padding-right", "15px");
     
     document.getElementsByClassName("navbar")[0].style.textAlign = "center";
     document.getElementById("desktop_menu").style.visibility = "collapse";
@@ -18,10 +21,13 @@ function Content_OnResize(){
     document.getElementById("send_review_button").style.visibility = "collapse";
   }
   else if(documentWidthProperty >= 750){
-    document.getElementById("content_div").style.paddingLeft = "75px";
+    //document.getElementById("content_div").style.paddingLeft = "75px";
     
     $('#navbar-logo').css("position", "relative");
-    $('#navbar-logo').css("left", "16px");
+      $('#navbar-logo').css("left", "16px");
+
+      $('#content-div-state').css("padding-left", "96px");
+      $('#content-div-state').css("padding-right", "96px");
     
     document.getElementsByClassName("navbar")[0].style.textAlign = "left";
     document.getElementById("desktop_menu").style.visibility = "visible";
