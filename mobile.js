d02e6817928d6043cc245481fe93a5f1feae8313
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2022-01-29 21:42:07 +0200

    fix

--- mobile.js
+++ mobile.js
@@ -26,12 +26,17 @@ function DetectMobileMode(){
 
       $('#page-title').css("font-size", "40px");
       $('#app-preview-image').css("top", "100px");
-        $('#app-preview-image').css("width", "85%");
+      $('#app-preview-image').css("width", "85%");
 
-        $('#content').css("width", "100%");
-        $('#apps-grid').css("width", "320px");
-        $('#apps-grid').css("padding-left", "20px");
-        $('#apps-grid').css("padding-right", "20px");
+      $('#content').css("width", "100%");
+      $('#apps-grid').css("width", "320px");
+      $('#apps-grid').css("padding-left", "20px");
+      $('#apps-grid').css("padding-right", "20px");
+
+      $('#microsoft-edge-text').css("padding-left", "35px");
+      $('#microsoft-edge-text').css("padding-right", "35px");
+      $('#microsoft-edge-text').css("top", "45px");
+      $('#microsoft-edge-content').css("padding-bottom", "135px");
     }
     else if(documentWidthProperty >= 1000){
       $('#navbar-logo').css("position", "relative");
@@ -45,53 +50,58 @@ function DetectMobileMode(){
 
       $('#page-title').css("font-size", "60px");
       $('#app-preview-image').css("top", "50px");
-        $('#app-preview-image').css("width", "1000px");
+      $('#app-preview-image').css("width", "1000px");
 
-        $('#content').css("width", "100%");
-        $('#apps-grid').css("width", "360px");
-        $('#apps-grid').css("padding-left", "0px");
-        $('#apps-grid').css("padding-right", "0px");
-      }
+      $('#content').css("width", "100%");
+      $('#apps-grid').css("width", "360px");
+      $('#apps-grid').css("padding-left", "0px");
+      $('#apps-grid').css("padding-right", "0px");
 
-      if (documentWidthProperty <= 880) {
-          $('#apps-description').css("padding-left", "35px");
-          $('#apps-description').css("padding-right", "35px");
-          $('#apps-description').css("margin-top", "40px");
-          $('#personalize-text').css("padding-left", "35px");
-          $('#personalize-text').css("padding-right", "35px");
-          $('#fast-actions-text').css("padding-left", "35px");
-          $('#fast-actions-text').css("padding-right", "35px");
-          $('#fast-actions-text').css("padding-top", "40px");
-          $('#fast-actions-text').css("top", "35px");
-          $('#get-gifts-text').css("padding-left", "35px");
-          $('#get-gifts-text').css("padding-right", "35px");
+      $('#microsoft-edge-text').css("padding-left", "135px");
+      $('#microsoft-edge-text').css("padding-right", "0px");
+      $('#microsoft-edge-text').css("top", "-75px");
+      $('#microsoft-edge-content').css("padding-bottom", "30px");
+    }
 
-          $('#whats-new-text').css("margin-left", "32px");
-          $('#whats-new-text').css("margin-right", "32px");
-          $('#whats-new-text').css("padding-top", "0px");
-          $('#whats-new-img').css("top", "12px");
+    if (documentWidthProperty <= 880) {
+        $('#apps-description').css("padding-left", "35px");
+        $('#apps-description').css("padding-right", "35px");
+        $('#apps-description').css("margin-top", "40px");
+        $('#personalize-text').css("padding-left", "35px");
+        $('#personalize-text').css("padding-right", "35px");
+        $('#fast-actions-text').css("padding-left", "35px");
+        $('#fast-actions-text').css("padding-right", "35px");
+        $('#fast-actions-text').css("padding-top", "40px");
+        $('#fast-actions-text').css("top", "35px");
+        $('#get-gifts-text').css("padding-left", "35px");
+        $('#get-gifts-text').css("padding-right", "35px");
 
-          $('bing-wallpapers').css("width", "100%");
+        $('#whats-new-text').css("margin-left", "32px");
+        $('#whats-new-text').css("margin-right", "32px");
+        $('#whats-new-text').css("padding-top", "0px");
+        $('#whats-new-img').css("top", "12px");
+
+        $('bing-wallpapers').css("width", "100%");
       }
       else if (documentWidthProperty >= 880) {
-          $('#apps-description').css("padding-left", "60px");
-          $('#apps-description').css("padding-right", "0px");
-          $('#apps-description').css("margin-top", "-50px");
-          $('#personalize-text').css("padding-left", "135px");
-          $('#personalize-text').css("padding-right", "0px");
-          $('#fast-actions-text').css("padding-left", "90px");
-          $('#fast-actions-text').css("padding-right", "0px");
-          $('#fast-actions-text').css("padding-top", "-35px");
-          $('#fast-actions-text').css("top", "-50px");
-          $('#get-gifts-text').css("padding-left", "100px");
-          $('#get-gifts-text').css("padding-right", "0px");
+        $('#apps-description').css("padding-left", "60px");
+        $('#apps-description').css("padding-right", "0px");
+        $('#apps-description').css("margin-top", "-50px");
+        $('#personalize-text').css("padding-left", "135px");
+        $('#personalize-text').css("padding-right", "0px");
+        $('#fast-actions-text').css("padding-left", "90px");
+        $('#fast-actions-text').css("padding-right", "0px");
+        $('#fast-actions-text').css("padding-top", "-35px");
+        $('#fast-actions-text').css("top", "-50px");
+        $('#get-gifts-text').css("padding-left", "100px");
+        $('#get-gifts-text').css("padding-right", "0px");
 
-          $('#whats-new-text').css("margin-left", "90px");
-          $('#whats-new-text').css("margin-right", "0px");
-          $('#whats-new-text').css("padding-top", "0px");
-          $('#whats-new-img').css("top", "-40px");
+        $('#whats-new-text').css("margin-left", "90px");
+        $('#whats-new-text').css("margin-right", "0px");
+        $('#whats-new-text').css("padding-top", "0px");
+        $('#whats-new-img').css("top", "-40px");
 
-          $('bing-wallpapers').css("width", "436px");
+        $('bing-wallpapers').css("width", "436px");
       }
   }
   catch(e){
