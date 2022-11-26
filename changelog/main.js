590f76f0c5d54aeed93158889860ca5ef38530b9
Dmitriy <winsoft.program@outlook.com>
2022-11-26 02:15:03 +0200

    What's new

---
+++ changelog/main.js
@@ -0,0 +1,32 @@
+﻿window.onload = function(){
+    Page_OnLoaded();
+};
+
+function Page_OnLoaded(){
+    try{
+
+    }
+    catch(e){
+        console.log(e.toString());
+    }
+}
+
+function SetTheme(theme){
+    try{
+        switch(theme){
+            case "Light":
+                document.body.style.backgroundColor = "rgb(246, 246, 246)";
+                document.body.style.color = "rgb(32, 32, 32)";
+                document.getElementsByClassName("gradient-div")[0].style.background = "linear-gradient(0deg, rgba(250, 250, 250, 1) 3%, rgba(255,255,255,0) 100%)";
+            break;
+            case "Dark":
+                document.body.style.backgroundColor = "rgb(32, 32, 32)";
+                document.body.style.color = "rgb(250, 250, 250)";
+                document.getElementsByClassName("gradient-div")[0].style.background = "linear-gradient(0deg, rgba(32, 32, 32, 1) 3%, rgba(255,255,255,0) 100%)";
+            break;
+        }
+    }
+    catch(e){
+        console.log(e.toString());
+    }
+}
